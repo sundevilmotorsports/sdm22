5b7d15730caeee3b41b5d026b2dbc62fc43c3b72
Joshua Tenorio <joshliam09@gmail.com>
2022-04-07 08:51:05 -0700

    changing things in cli-test

---
+++ tests/cli-test/src/Logger.h
@@ -0,0 +1,47 @@
+/**
+ * @file Logger.h
+ * @author Joshua Tenorio
+ * @brief Contains prototypes for Logger class
+ */
+#pragma once
+
+#include <Arduino.h>
+#include <SD.h>
+#include <string>
+#include <vector>
+#include <map>
+
+// data structure used to represent a csv file
+// name: complete filename w/ path
+// columns: what the columns are named. includes timestamp by default
+// currentRow: the current row we're making
+typedef struct SDMFile_s_t {
+    std::string name;
+    std::map<std::string, size_t> columns;
+    std::vector<float> currentRow;
+} SDMFile;
+
+class Logger {
+    public:
+    Logger();
+
+    // TODO: move functions in Logger to here
+    void setup();
+
+    // creates a .csv file and SDMFile object with desired columns
+    void initializeFile(std::string filename, std::vector<std::string> columns);
+
+    // adds a data entry to filename's column
+    // filename: name of the file, doesnt need to be the complete path
+    bool addData(std::string filename, std::string column, float data);
+
+    // writes currentRow into the actual csv file
+    bool writeRow(std::string filename);
+
+    protected:
+    // folder to write into
+    std::string path;
+
+    // hashmap for SDMFile objects
+    std::map<std::string, SDMFile> files;
+};
