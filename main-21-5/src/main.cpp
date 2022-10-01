05b1bcca753691d46ff5853d96de060c3f005ef2
Joshua Tenorio <joshliam09@gmail.com>
2022-09-30 20:59:24 -0700

    sdm21.5 main board code

---
+++ main-21-5/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <Accelerometer.h>
+#include <SDMSerial.h>
+#include <Logger.h>
+
+Logger logger;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
