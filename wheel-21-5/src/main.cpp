bd5526703f776a03824d9eed0be35cd34d9cc497
Joshua Tenorio <joshliam09@gmail.com>
2022-09-30 21:08:42 -0700

    sdm21.5 wheel board code for platformio

---
+++ wheel-21-5/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <LinearPot.h>
+#include <SDMSerial.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
